8698321d884a6691501f16b51cc2330e38b6fd04
VitaliiFedin <vitalii.fedinn@gmail.com>
2024-01-30 23:31:51 +0200

    updated: states.py; docker-compose

--- states.py
+++ states.py
@@ -2,11 +2,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 
 
 class Form(StatesGroup):
-    """_summary_
-
-    Args:
-        StatesGroup (_type_): _description_
-    """
+    """Represents the states used in the Telegram bot conversation."""
 
     location = State()
     checklist_item = State()
